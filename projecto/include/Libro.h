69dcbc0737c72d795bdc6034827f64c7845718b5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-07-04 12:44:25 -0500

    feat: add Libro.h and update readme

---
+++ projecto/include/Libro.h
@@ -0,0 +1,31 @@
+#ifndef LIBRO_H
+#define LIBRO_H
+
+#include "MaterialBibliografico.h"
+
+using std::string;
+
+class Libro : public MaterialBibliografico
+{
+private:
+  string genero;
+  int numeroPaginas;
+
+public:
+  Libro(const string &titulo, const string &autor, const string &isbn,
+        int anio, int copias, const string &genero = "", int paginas = 0);
+
+  void mostrarInformacion() const override;
+  string getTipo() const override { return "LIBRO"; }
+  string toFileString() const override;
+
+  string getGenero() const { return genero; }
+  int getNumeroPaginas() const { return numeroPaginas; }
+
+  void setGenero(const string &genero) { this->genero = genero; }
+  void setNumeroPaginas(int paginas) { this->numeroPaginas = paginas; }
+
+  static Libro *fromFileString(const string &line);
+};
+
+#endif // LIBRO_H
